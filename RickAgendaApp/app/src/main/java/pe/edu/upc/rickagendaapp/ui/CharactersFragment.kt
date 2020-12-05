611ecb8f38229bba6ac55caf8832154cee31ae5f
alonzoDIEGO <dhilariocallupe@gmail.com>
2020-12-05 17:21:12 -0500

    Implementacion navbar con Navigation

---
+++ RickAgendaApp/app/src/main/java/pe/edu/upc/rickagendaapp/ui/CharactersFragment.kt
@@ -0,0 +1,25 @@
+package pe.edu.upc.rickagendaapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_characters.*
+import pe.edu.upc.rickagendaapp.R
+
+class CharactersFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_characters, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btNext.setOnClickListener {
+            findNavController().navigate(R.id.characterDetailFragment)
+        }
+    }
+}
